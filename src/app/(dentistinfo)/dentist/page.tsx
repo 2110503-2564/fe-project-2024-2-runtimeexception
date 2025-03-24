bbfa30498e92fd04a9bd70cdf8b642b4d49dda34
Theerapat Jongpatrakul <6733113621@student.chula.ac.th>
2025-03-24 16:28:37 +0700

    /dentist

--- src/app/(dentistinfo)/dentist/page.tsx
+++ src/app/(dentistinfo)/dentist/page.tsx
@@ -2,18 +2,15 @@ import getDentists from "@/libs/getDentists"
 import DentistCatalog from "@/components/DentistCatalog"
 import { Suspense } from "react"
 import { LinearProgress } from "@mui/material"
-import DentistPanel from "@/components/DentistPanel"
-export default  function Dentist(){
+import CarPanel from "@/components/CarPanel"
+export default  function Car(){
     const dentists = getDentists()
     return(
         <main className="text-center p-5">
-            <h1 className="text-xl font-medium">Select Your Travel Partner</h1>
+            <h1 className="text-xl font-medium">Select Your Dentist</h1>
             <Suspense fallback={<p>Loading...<LinearProgress></LinearProgress></p>}>
             <DentistCatalog dentistJson={dentists}/>
        </Suspense>
-       <hr className="my-10"/>
-       <h1 className="text-xl font-medium">TRY Client-Side Car Panel</h1>
-        <DentistPanel/>
         </main>
     )
 }
--- src/libs/getDentists.tsx
+++ src/libs/getDentists.tsx
@@ -1,5 +1,5 @@
 export default async function getDentists(){
-    await new Promise((resolve)=>setTimeout(resolve,5000))
+    await new Promise((resolve)=>setTimeout(resolve,1000))
     const response = await fetch("http://localhost:5000/api/v1/dentists",{next:{tags:['dentists']}})
     if(!response.ok){
         throw new Error("Failed to fetch Dentists")
