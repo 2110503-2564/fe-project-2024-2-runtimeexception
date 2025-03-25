0e48ece9ac854b97a62aa17f7a0857fbd46f92c5
RyuJirayu123 <rew254967@gmail.com>
2025-03-25 12:50:31 +0700

    change for vercel

--- src/libs/addBooking.tsx
+++ src/libs/addBooking.tsx
@@ -1,5 +1,5 @@
 export default async function addBooking(dentId:string,date:string){
-    const responses = await fetch(`http://localhost:5000/api/v1/auth/dentists/${dentId}`,{
+    const responses = await fetch(`${process.env.BACKEND_URL}/api/v1/auth/dentists/${dentId}`,{
         method:"POST",
         headers:{
             "Content-Type":"application/json",
--- src/libs/deleteBooking.tsx
+++ src/libs/deleteBooking.tsx
@@ -1,6 +1,6 @@
 export default async function deleteBooking(token: string, id: string){
 
-    const response = await fetch(`http://localhost:5000/api/v1/bookings/${id}`,{
+    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/bookings/${id}`,{
         method:"DELETE",
         headers:{
             "Authorization": `Bearer ${token}`
